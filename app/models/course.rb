07cc56cd24a7102275064b213249e088a09c7d5f
Elly <ellychau10@yahoo.com>
2021-04-13 10:25:53 -0700

    Final Commit

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,32 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  instructor_id :integer
+#  prereq_id     :integer
+#
 class Course < ApplicationRecord
+    has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+    has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
+
+    belongs_to :prerequisite,
+    class_name: :Course,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    optional: true
+
+    belongs_to :instructor,
+    class_name: :User,
+    primary_key: :id,
+    foreign_key: :instructor_id
 end
